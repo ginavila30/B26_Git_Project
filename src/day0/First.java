d09a2a504e6edb0b0543ef9a1882ab4646eff25e
Gina <ginavila30@icloud.com>
2022-03-26 16:07:07 -0400

    Updated First Class

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,5 +3,9 @@ package day0;
 public class First {
     public static void main(String[] args) {
         //TODO: Work on this tomorrow, add some code.
+
+        //Extra line 1
+        //Extra line 2
+        //Extra line 3
     }
 }
